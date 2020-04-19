04d88819ad7db870587c1008f68deb9e763d18aa
zh_haining <zh_haining@163.com>
2020-04-19 17:08:52 +0800

    the log add uuid and add the file of jenkinsfiles

--- src/main/java/com/snpx/jenkinsdemo/control/TestController.java
+++ src/main/java/com/snpx/jenkinsdemo/control/TestController.java
@@ -3,12 +3,15 @@ package com.snpx.jenkinsdemo.control;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 public class TestController {
 
     @GetMapping(value = "/hello")
     public String sayHello(String name){
-        String msg = String.format("Hi %s, hello world",name);
+        String uuid = UUID.randomUUID().toString().toUpperCase();
+        String msg = String.format("Hi %s, hello world | %s",name,uuid);
         System.out.println(msg);
         return msg;
     }
